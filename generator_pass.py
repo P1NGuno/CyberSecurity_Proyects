e85f5d3f5f60ce97f21a6acc5b8d97f51aafa24d
pinguno <r4f4hc8@gmail.com>
2025-05-27 22:33:57 +0200

    Implementacion de la funcion de solicitar

--- generator_pass.py
+++ generator_pass.py
@@ -1,14 +1,22 @@
+def generator_pass (letras_min = True , letras_mayus = True, num = True , caract_espec = True):
+    print(f"minusculas {letras_min} mayusculas {letras_mayus} num {num} caracteristicas especiales {caract_espec}")
+
 def main():
     try:
-        long = int(input("Ingrese la longitud de la contraseña que desea. \nEsta debe ser mayor que 8 \n"))
-
-        if long <= 8:
-            print("La longitud debe de ser 8")
-        else:
-            print("Correcto")
-
+        longitud = int(input("Longitud de la contraseña (mínimo 8): "))
+        if longitud < 8:
+            raise ValueError("La longitud mínima es 8.")
     except ValueError as e:
-        print(f"Error {e}")
+        print(f"Error: {e}")
+        return
+
+    letras_min = input("Quiere ingresar letras minusculas?: s/n ").lower() == "s"
+    letras_mayus = input("Quiere ingresar letras mayusculas?: s/n ").lower() == "s"
+    num = input("Quiere ingresar numeros?: s/n ").lower() == "s"
+    caract_espec = input("Quiere ingresar caracteres especiales?: s/n ").lower() == "s"
+    generator_pass(letras_min, letras_mayus, num , caract_espec)
+
+    
 
 if __name__ == "__main__":
     main()
